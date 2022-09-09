f1b50f8f4762f05a3f42181a3496f0d25c8afb9f
Uros <ukalabic@protonmail.com>
2022-09-10 01:47:11 +0200

    Updated WiHi page

--- src/PageWiHi.svelte
+++ src/PageWiHi.svelte
@@ -1,13 +1,55 @@
 <script>
     import WhitePaper from './assets/wihi_whitepaper_160822.pdf'
     import Presentation from './assets/wihi_presentation_160822.pdf'
+    import MicroSchematic from './assets/how_wihi_works.png'
 </script>
+
 
 
 <h1>Welcome to WiHi</h1>
 
-<h2>Click a link below:</h2>
+<div style="width:600px;text-align:center;display: inline-block;">
+<p>
+  WiHi is a decentralized solution to the problems of weather forecasting and climate monitoring.
+  It comes with an algorithm that measures contributions to forecast improvement and pays out participants in proportion to how much improvement their sensors provide.
+</p>
 
+<h2>How does it work?</h2>
+
+<p>
+WiHi rewards data providers in proportion to how much their data improve the forecast.
+The rewards are computed according to a "post-processing" algorithm that assimilates forecasts
+(based on publicly available data) with WiHi sensor data.
+</p>
+
+<img src={MicroSchematic} style="width:550px">
+</div>
+
+
+<h2>Why do I want to use WiHi?</h2>
+
+<div style="width:600px;text-align:left;display: inline-block;">
+  <p>
+  If you are a manufacturer of weather sensors...
+  <br><span style="float:right;">...WiHi increases the value of your product.</span>
+  <br>
+</p>
+
+<p>
+  If you are looking to run a    sensor...
+  <br><span style="float:right;">...WiHi pays you for your data.</span>
+  <br>
+</p>
+
+<p>
+  If you are the general public...
+  <br><span style="float:right;">...WiHi improves our understanding of weather (and gives better forecasts).</span>
+</p>
+</div>
+
+<h2>Click a link below to learn more:</h2>
+
+<span style="font-size:2em">
 <p>
 <a href="https://vimeo.com/740243385" target="_blank">presentation</a>
 </p>
@@ -19,9 +61,10 @@
 <p>
 <a href={WhitePaper} target="_blank">whitepaper</a>
 </p>
+</span>
 
-<style>
+<!--<style>
     p {
       font-size: 3em;
     }
-</style>  
+</style>  -->
